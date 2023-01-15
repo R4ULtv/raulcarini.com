72eb1f3be3cdcbfd4e0a06dbf752ec0314f2fed7
Raul Carini <r.carini2003@gmail.com>
2023-01-15 11:59:59 +0100

    small change

--- components/Navigation.js
+++ components/Navigation.js
@@ -47,14 +47,14 @@ export default function Navigation() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            {/* <a href="#">
               <span className="sr-only">Raul Carini</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src="/assets/svg/logo.svg"
                 alt=""
               />
-            </a>
+            </a> */}
           </div>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
