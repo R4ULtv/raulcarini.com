47cd6abf35097dc7e590feb32ec38ecbfab60c01
Raul Carini <r.carini2003@gmail.com>
2023-01-18 20:39:12 +0100

    fix small animations

--- components/Projects.js
+++ components/Projects.js
@@ -8,7 +8,7 @@ export default function Projects(data) {
                 {data.images.map((item) => (
                     <a key={item.url} href={item.url} target="_blank" className="relative text-center group flex items-center justify-center">
                         <Image src={item.src} width="320" height="320" quality="100" className="h-80 rounded-lg md:group-hover:scale-110 md:group-hover:blur-sm duration-150" alt={item.title}/>
-                        <div className="group-hover:opacity-100 duration-150 opacity-0 absolute m-0 text-white font-bold text-5xl">{item.title}</div>
+                        <div className="group-hover:opacity-100 group-hover:scale-90 text-5xl  duration-150 opacity-0 absolute m-0 text-white font-bold ">{item.title}</div>
                     </a>
                 ))}
             </div>
