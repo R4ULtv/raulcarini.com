89cb764fc66a661b974bd4dc0dbf602a202702c0
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-05 14:00:58 +0200

    redirect to new website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://www.raulcarini.dev',
+        "statusCode": 301
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
